1c17e2c4b30f8ac87e8b96e76aaba69b877ec7b4
Shivangi <developer@spontivly.com>
2022-06-08 02:44:16 -0600

    creating products context and product card

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+
+import { ProductsContext } from '../../contexts/products.context';
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './shop.styles.scss';
+
+// import SHOP_DATA from '../../shop-data.json';
+
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+  console.log(products);
+
+  return (
+    <div className='products-container'>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default Shop;
